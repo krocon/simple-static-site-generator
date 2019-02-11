b66023deef46560b7dc3485e5a220eff7a426d44
Marc Kronberg <krocon@gmail.com>
2019-02-11 19:57:53 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # simple-static-site-generator
 simple static site generator (serverless html)
+
+`npm run serve`
---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+const gulp = require("gulp");
+const del = require('del');
+const browsersync = require("browser-sync").create();
+const headerfooter = require('gulp-headerfooter');
+
+
+// BrowserSync
+function startBrowsersync(done) {
+    browsersync.init({
+        server: {
+            baseDir: "dist/"
+        },
+        port: 3000
+    });
+    done();
+}
+
+
+// BrowserSync Reload
+function reloadBrowserSync(done) {
+    browsersync.reload();
+    done();
+}
+
+
+// Watch files
+function watchFiles() {
+    gulp.watch("./dist/**/*", reloadBrowserSync);
+    gulp.watch("./src/**/*", cleanCopyAssetsAndBuild);
+    gulp.watch("./index.html", reloadBrowserSync);
+}
+
+// Clean dist folder
+function clean(done) {
+    console.info('Cleaning...');
+    return del(["./dist/**/*"]);
+    done();
+}
+
+// copy assets:
+function copyAssets(done) {
+    console.info('copy assets...');
+    return gulp
+        .src(['src/assets/**/*'], {base: 'src/assets'})
+        .pipe(gulp.dest('dist/assets'));
+    done();
+}
+
+// build: concatinate for each html  header & content & footer:
+function build(done) {
+    console.info('build...');
+    gulp
+        .src('src/**/*.html')
+        .pipe(headerfooter.header('src/header.html'))
+        .pipe(headerfooter.footer('src/footer.html'))
+        .pipe(gulp.dest('dist'));
+    done();
+}
+
+const cleanCopyAssetsAndBuild = gulp.series(clean, copyAssets, build);
+const serve = gulp.parallel(cleanCopyAssetsAndBuild, watchFiles, startBrowsersync);
+
+
+exports.default = build;
+exports.clean = clean;
+exports.copyAssets = copyAssets;
+exports.build = build;
+exports.cleanCopyAssetsAndBuild = cleanCopyAssetsAndBuild;
+
+exports.serve = serve;
+exports.watchFiles = watchFiles;
